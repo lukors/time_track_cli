cf3ead12260b02337d3cb7405cd91804bfb24acb
Lukas Orsvärn <lukas@orsvarn.com>
2018-06-06 09:18:23 +0200

    Can now configure where the db is

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,22 @@ use std::{fs::File,
 		  io::{self, prelude::*},
 		  path::Path};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 struct Config {
 	path: String,
 }
+const CONFIG_PATH: &str = "config.json";
 
 impl Config {
 	fn read() -> io::Result<Config> {
-		let file = File::open("config.json")?;
+		let file = File::open(CONFIG_PATH)?;
 		let config = serde_json::from_reader(file)?;
 		Ok(config)
 	}
 
 	fn write(&self) -> io::Result<()> {
-		let file = File::open("config.json")?;
-		let config = serde_json::from_reader(file)?;
+		let file = File::create(CONFIG_PATH)?;
+		serde_json::to_writer_pretty(&file, self)?;
 		Ok(())
 	}
 }
@@ -42,18 +43,22 @@ fn main() {
 			.about("Edit the config file")
 			.arg(Arg::with_name("path")
 				.short("p")
+				.long("path")
 				.help("Set the path of the database file")
+				.value_name("FILE")
+				.takes_value(true)
 			)
 		)
 		.get_matches();
 
-		let cfg = Config::read().unwrap();
-		if let Some(matches) = matches.subcommand_matches("add") {
-			add_event(&cfg);
-		}
-		if let Some(matches) = matches.subcommand_matches("config") {
-			config();
-		}
+	let cfg = Config::read().unwrap();
+
+	if let Some(matches) = matches.subcommand_matches("add") {
+		add_event(&cfg);
+	}
+	if let Some(matches) = matches.subcommand_matches("config") {
+		config(matches, &cfg).unwrap();
+	}
 }
 
 fn add_event(config: &Config) -> io::Result<()> {
@@ -61,7 +66,15 @@ fn add_event(config: &Config) -> io::Result<()> {
 	Ok(())
 }
 
-fn config() {
+fn config(matches: &clap::ArgMatches, config: &Config) -> io::Result<()> {
+	let mut config_new = config.clone();
 
+	if let Some(path) = matches.value_of("path") {
+		config_new.path = path.to_string();
+	}
+
+	config_new.write()?;
+
+	Ok(())
 }
 
